765623d033b36902dbedd06c1010c4a269332c2a
Thomas Bockhorn <thomas.bockhorn@tecktonet.com>
2022-01-21 20:15:14 -0600

    Added a splash screen

--- frontend/thomas_portfolio_front/src/App.vue
+++ frontend/thomas_portfolio_front/src/App.vue
@@ -1,21 +1,38 @@
 <template>
     <main>
-        <Header />
-        <routerView></routerView>
-        <Footer />
+        <div v-if="isLoading">
+            <LoadingScreen v-if="isLoading"></LoadingScreen>
+        </div>
+        <div v-if="!isLoading">
+            <Header/>
+            <routerView></routerView>
+            <Footer/>
+        </div>
     </main>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
+import LoadingScreen from "./components/pages/LoadingScreen.vue";
 
 export default {
     name: "App",
     components: {
         Header,
         Footer,
+        LoadingScreen
     },
+    data(){
+        return{
+            isLoading: true
+        }
+    },
+    mounted(){
+        setTimeout(() => {
+            this.isLoading = false
+        }, 3000)
+    }
 };
 </script>
 
