6265d61dbff087f58434740adb24e0a7b5e973b5
Thomas Bockhorn <thomas.bockhorn@tecktonet.com>
2022-01-23 21:57:21 -0600

    added a video to landing page

--- frontend/thomas_portfolio_front/src/components/pages/LandingPage.vue
+++ frontend/thomas_portfolio_front/src/components/pages/LandingPage.vue
@@ -1,13 +1,29 @@
 <template>
-    <div>
-       Landing page.... 
-    </div>
+    <div class="flex-container">
+        <div class="videoPlayer">
+            <video autoplay muted id="video">
+            <source src="../../assets/mov/Atoms.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+            </video> 
+        </div>
+    </div>  
 </template>
 <script>
 export default {
     
 }
 </script>
-<style>
-    
+<style scoped>
+    #video{
+        position: absolute;
+        width: 100%;
+        height: auto;
+    }
+
+    .flex-container{
+        height: 100%;
+        display: flex;
+        background-color: black;
+        position: relative;
+    }
 </style>
