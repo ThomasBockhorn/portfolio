64eb6369e4bb8ea87b01669c2211a969d93b43b5
Thomas Bockhorn <thomas.bockhorn@tecktonet.com>
2022-01-30 14:21:48 -0600

    Added portrait and text area in About

---
+++ frontend/thomas_portfolio_front/src/components/components/AboutComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-3">
+                <img src="../../assets/ThomasBockhorn.jpg" alt="self image">
+            </div>
+            <div class="col-md-auto">
+                <h1>About Me</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+img{
+    width: 100%;
+    height: 100%;
+}
+</style>
--- frontend/thomas_portfolio_front/src/components/pages/Home.vue
+++ frontend/thomas_portfolio_front/src/components/pages/Home.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <MainBanner></MainBanner>
+        <AboutComponent></AboutComponent>
     </div>
 </template>
 
 <script>
 import MainBanner from "../components/MainBanner.vue";
+import AboutComponent from "../components/AboutComponent.vue";
 
 export default {
     components:{
-        MainBanner
+        MainBanner,
+        AboutComponent
     }
 
 };
