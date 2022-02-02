cb6a77f34bcbb2e4eed3315cb823895aba923ac0
Thomas Bockhorn <thomas.bockhorn@tecktonet.com>
2022-02-02 00:08:35 -0600

    Added about me description and font awesome

--- frontend/thomas_portfolio_front/src/components/Header.vue
+++ frontend/thomas_portfolio_front/src/components/Header.vue
@@ -1,36 +1,58 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top bg-transparent">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#"></a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link to="/home" class="nav-link" aria-current="Home">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/resume" class="nav-link" aria-current="Resume">Resume</router-link>
-          </li>
-         <li class="nav-item">
-           <router-link to="/portfolio" class="nav-link" aria-current="Project">Projects</router-link>
-         </li>
-          <li class="nav-item">
-            <router-link to="/contact" class="nav-link" href="#" aria-current="page">Contact</router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+    <nav
+        class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top bg-transparent"
+    >
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"></a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link
+                            to="/home"
+                            class="nav-link"
+                            aria-current="Home"
+                            >Home</router-link
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <router-link
+                            to="/resume"
+                            class="nav-link"
+                            aria-current="Resume"
+                            >Resume</router-link
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <router-link
+                            to="/portfolio"
+                            class="nav-link"
+                            aria-current="Project"
+                            >Projects</router-link
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <router-link
+                            to="/contact"
+                            class="nav-link"
+                            aria-current="Contact"
+                            >Contact</router-link
+                        >
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
@@ -38,9 +60,8 @@ export default {};
 </script>
 
 <style scoped>
-  ul li.router-link-active {
+ul li.router-link-active {
     color: white;
     cursor: pointer;
-  }
-
+}
 </style>
--- frontend/thomas_portfolio_front/src/components/components/AboutComponent.vue
+++ frontend/thomas_portfolio_front/src/components/components/AboutComponent.vue
@@ -1,13 +1,40 @@
 <template>
-    <div class="container mt-2">
-        <div id="about" class="row justify-content-md-center">
+    <div class="container-fluid mt-2">
+        <div id="about" class="row">
             <div class="col-lg-3">
-                <img src="../../assets/ThomasBockhorn.jpg" alt="self image">
+                <img src="../../assets/ThomasBockhorn.jpg" alt="self image" />
             </div>
-            <div class="col-md-auto mt-2">
+            <div class="col-lg-9 mt-2">
                 <h1>About Me</h1>
-                <hr/>
-                <p>Hi, my name is Thomas Bockhorn.  I'm a full stack web developer.</p>
+                <hr />
+                <p>
+                    Hi, my name is Thomas Bockhorn. I'm a full stack web
+                    developer. Hi Building websites are fun and challanging.
+                    Websites can also be creative endevors that inspire.
+                </p>
+                <p>
+                    I first got into web development with an invitation by
+                    Udacity including a nice Google scholarship for their
+                    front-end web development program. After taking a few
+                    lessions, I became hooked on all things web development. To
+                    further my education, I took and continue to take Udemy
+                    courses for both front-end and back-end development. The
+                    beauty of both technical and asthetic spheres and how they
+                    interacted together fascinated me. Just like architecture,
+                    good websites need to consider both form and function.
+                </p>
+                <p>
+                    My other source of inspiration is history and the art of
+                    telling stories. I received a Master degree in history. Good
+                    historical narratives and websites share how important the
+                    central theme is and how essential the human connection is.
+                    History and websites can be dry and boring, or can be
+                    interesting and compelling. It all depends on the content. I
+                    try to make compelling websites that garner viewer's
+                    attention.
+                </p>
+                <p>I work with Java, PHP, Laravel, CSS, HTML, Vue, MySql.</p>
+                <p>-Thomas Bockhorn</p>
             </div>
         </div>
     </div>
@@ -18,7 +45,7 @@ export default {};
 </script>
 
 <style scoped>
-img{
+img {
     width: 100%;
     height: 100%;
 }
