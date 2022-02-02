c1d6b4737387ba8848d0d1075ba109762cc177f3
Thomas Bockhorn <thomas.bockhorn@tecktonet.com>
2022-02-02 00:16:12 -0600

    fixed about paragraphs

--- frontend/thomas_portfolio_front/src/components/components/AboutComponent.vue
+++ frontend/thomas_portfolio_front/src/components/components/AboutComponent.vue
@@ -9,29 +9,29 @@
                 <hr />
                 <p>
                     Hi, my name is Thomas Bockhorn. I'm a full stack web
-                    developer. Hi Building websites are fun and challanging.
-                    Websites can also be creative endevors that inspire.
+                    developer. Building websites and web applications is fun and
+                    challenging. Websites can also be creative endevors that
+                    inspire.
                 </p>
                 <p>
                     I first got into web development with an invitation by
-                    Udacity including a nice Google scholarship for their
-                    front-end web development program. After taking a few
-                    lessions, I became hooked on all things web development. To
-                    further my education, I took and continue to take Udemy
-                    courses for both front-end and back-end development. The
-                    beauty of both technical and asthetic spheres and how they
-                    interacted together fascinated me. Just like architecture,
-                    good websites need to consider both form and function.
+                    Udacity including a Google scholarship for their front-end
+                    web development program. After taking a few lessions, I
+                    became hooked on all things web development. To further my
+                    education, I took and continue to take Udemy courses for
+                    both front-end and back-end development. The beauty of both
+                    technical and asthetic spheres and how they interacted
+                    together fascinated me. Just like architecture, good
+                    websites need to consider both form and function.
                 </p>
                 <p>
                     My other source of inspiration is history and the art of
-                    telling stories. I received a Master degree in history. Good
-                    historical narratives and websites share how important the
-                    central theme is and how essential the human connection is.
-                    History and websites can be dry and boring, or can be
+                    telling stories. I received a Master's degree in history.
+                    Good historical narratives and websites share how important
+                    the central theme is and how essential the human connection
+                    is. History and websites can be dry and boring, or can be
                     interesting and compelling. It all depends on the content. I
-                    try to make compelling websites that garner viewer's
-                    attention.
+                    make compelling websites that garner viewer's attention.
                 </p>
                 <p>I work with Java, PHP, Laravel, CSS, HTML, Vue, MySql.</p>
                 <p>-Thomas Bockhorn</p>
