ca93973c95a672cf7e4a199686786b3ce2e041fe
Thomas Bockhorn <thomas.bockhorn@tecktonet.com>
2022-02-05 12:16:57 -0600

    Added resume

--- frontend/thomas_portfolio_front/src/components/components/ResumeComponent.vue
+++ frontend/thomas_portfolio_front/src/components/components/ResumeComponent.vue
@@ -11,6 +11,82 @@
         <div class="row">
             <div class="col"><h2>Full Stack Web Developer</h2></div>
         </div>
+        <div class="row mt-5">
+            <div class="col"></div>
+            <div class="col">
+                <div class="row">
+                    <div><h3>Skills</h3></div>
+                    <div>
+                        <p>
+                            PHP, Laravel, Javascript, HTML 5, CSS 3, GitHub,
+                            Git, VueJS, JQuery, WordPress, MySQL, OOP, PHPUnit,
+                            Java, Spring
+                        </p>
+                    </div>
+                    <div><h3>Awards</h3></div>
+                    <div>
+                        <p>
+                            Udacity Core Java Certification, Udemy web
+                            development certifications
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div><h3>Experience</h3></div>
+                <div><h4>Full Stack Web Developer / Freelancer</h4></div>
+                <div><h5>1/1/2019 - Present</h5></div>
+                <div>
+                    <p>
+                        Full stack Web Developer providing services aimed to
+                        help my clients achieve their goals. I also enjoy the
+                        challenge of building and improving websites.
+                    </p>
+                </div>
+                <div class="mt-5"><h4>Wholefoods Market, Huntsville Alabama</h4></div>
+                <div><h5>November 2016 - Present</h5></div>
+                <div>
+                    <p>
+                        I work in the meat/seafood department in Wholefoods. I
+                        provide customer service and follow all the protocols
+                        that the job requires.
+                    </p>
+                </div>
+                <div class="mt-5"><h4>Walmart, Huntsville Alabama</h4></div>
+                <div><h5>April 2010 - November 2016</h5></div>
+                <div>
+                    <p>
+                        Provided customer service, promoted as department manager.
+                    </p>
+                </div>
+            </div>
+            <div class="col"></div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="mt-5"><h3>Education</h3></div>
+                <div class="mt-3"><h4>Udacity - Core Java</h4></div>
+                <div><p>2021</p></div>
+                <div class="mt-3"><h4>Udacity - Front-End Web Development</h4></div>
+                <div><p>2018</p></div>
+                <div class="mt-3"><h4>Udemy - Web Development Courses</h4></div>
+                <div><p>2017</p></div>
+                <div class="mt-3"><h4>University of Alabama, Huntsville - Master Degree</h4></div>
+                <div><p>2010 - History Degree</p></div>
+                <div class="mt-3"><h4>Montclair State University - Bachelor Degree</h4></div>
+                <div><p>2001 - Computer Science</p></div>
+            </div>
+            <div class="col"></div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="mt-5"><h3>Projects</h3></div>
+                <div><p>See Github: <a href="https://github.com/ThomasBockhorn">https://github.com/ThomasBockhorn</a></p></div>
+            </div>
+            <div class="col"></div>
+        </div>
     </div>
 </template>
 
