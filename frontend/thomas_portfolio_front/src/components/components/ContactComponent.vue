2502e11c56c54a1fb53d6b2473f0e9ecec0674b1
Thomas <thomas.bockhorn@tecktonet.com>
2022-02-11 04:22:17 -0600

    Added a contact form

---
+++ frontend/thomas_portfolio_front/src/components/components/ContactComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col text-center">
+        <h3>Contact Form</h3>
+      </div>
+      <form class="row g-3">
+        <div class="col-md-6">
+          <label for="firstName" class="form-label">First Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="firstName"
+            placeholder="Theodore"
+          />
+        </div>
+        <div class="col-md-6">
+          <label for="lastName" class="form-label">Last Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="lastName"
+            placeholder="Roosevelt"
+          />
+        </div>
+        <div class="col-12">
+          <label for="email" class="form-label">Email</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            placeholder="Theodore.Roosevelt@Bully.com"
+          />
+        </div>
+        <div class="col-12">
+          <label for="message" class="form-label">Message</label>
+          <div class="form-floating">
+            <textarea
+              class="form-control"
+              placeholder="Leave a comment here"
+              id="message"
+              style="height: 100px"
+            ></textarea>
+            <label for="message">Enter your message here</label>
+          </div>
+        </div>
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary">Send</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
--- frontend/thomas_portfolio_front/src/components/pages/Contact.vue
+++ frontend/thomas_portfolio_front/src/components/pages/Contact.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <MainBanner></MainBanner>
+        <ContactComponent></ContactComponent>
     </div>
 </template>
 
 <script>
 import MainBanner from "../components/MainBanner.vue";
+import ContactComponent from "../components/ContactComponent.vue";
 
 export default {
     components:{
-        MainBanner
+        MainBanner,
+        ContactComponent
     }
 };
 </script>
