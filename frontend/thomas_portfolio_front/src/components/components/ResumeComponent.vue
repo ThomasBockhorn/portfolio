f1ccca13d02dda64a4cb046a264fdd68890504ba
Thomas <thomas.bockhorn@tecktonet.com>
2022-02-11 19:14:19 -0600

    Added endoresment / added links social media

--- frontend/thomas_portfolio_front/src/components/components/ResumeComponent.vue
+++ frontend/thomas_portfolio_front/src/components/components/ResumeComponent.vue
@@ -1,93 +1,115 @@
 <template>
-    <div id="resume" class="container mt-5">
-        <div class="row">
-            <div class="col-md-12"><h1>Thomas Bockhorn</h1></div>
-            <div class="col-md-12">
-                <div>113 Sweet Hollow Way NW</div>
-                <div>Madison, AL 35757</div>
-                <div>Thomas.Bockhorn@tecktonet.com</div>
-            </div>
-        </div>
-        <div class="row mt-5">
-            <div class="col-md-12 text-center"><h2>Full Stack Web Developer</h2></div>
-        </div>
-        <div class="row mt-5">
-            <div class="col"></div>
-            <div class="col-md-6">
-                <div class="row">
-                    <div><h3>Skills</h3></div>
-                    <div>
-                        <p>
-                            PHP, Laravel, Javascript, HTML 5, CSS 3, GitHub,
-                            Git, VueJS, JQuery, WordPress, MySQL, OOP, PHPUnit,
-                            Java, Spring
-                        </p>
-                    </div>
-                    <div><h3>Awards</h3></div>
-                    <div>
-                        <p>
-                            Udacity Core Java Certification, Udemy web
-                            development certifications
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <div class="mt-5"><h3>Experience</h3></div>
-                <div class="mt-5"><h4>Full Stack Web Developer / Freelancer</h4></div>
-                <div><h5>1/1/2019 - Present</h5></div>
-                <div>
-                    <p>
-                        Full stack Web Developer providing services aimed to
-                        help my clients achieve their goals. I also enjoy the
-                        challenge of building and improving websites.
-                    </p>
-                </div>
-                <div class="mt-5"><h4>Whole Foods Market, Huntsville Alabama</h4></div>
-                <div><h5>November 2016 - Present</h5></div>
-                <div>
-                    <p>
-                        I work in the meat/seafood department in Wholefoods. I
-                        provide customer service and follow all the protocols
-                        that the job requires.
-                    </p>
-                </div>
-                <div class="mt-5"><h4>Walmart, Huntsville Alabama</h4></div>
-                <div><h5>April 2010 - November 2016</h5></div>
-                <div>
-                    <p>
-                        Provided customer service, promoted as department manager.
-                    </p>
-                </div>
-            </div>
-            <div class="col"></div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <div class="mt-5"><h3>Education</h3></div>
-                <div class="mt-5"><h4>Udacity - Core Java</h4></div>
-                <div><p>2021</p></div>
-                <div class="mt-3"><h4>Udacity - Front-End Web Development</h4></div>
-                <div><p>2018</p></div>
-                <div class="mt-3"><h4>Udemy - Web Development Courses</h4></div>
-                <div><p>2017</p></div>
-                <div class="mt-3"><h4>University of Alabama, Huntsville - Master Degree</h4></div>
-                <div><p>2010 - History Degree</p></div>
-                <div class="mt-3"><h4>Montclair State University - Bachelor Degree</h4></div>
-                <div><p>2001 - Computer Science</p></div>
-            </div>
-            <div class="col"></div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <div class="mt-5"><h3>Projects</h3></div>
-                <div class="mt-3"><p>See Github: <a href="https://github.com/ThomasBockhorn">https://github.com/ThomasBockhorn</a></p></div>
-            </div>
-            <div class="col"></div>
-        </div>
+  <div id="resume" class="container mt-5">
+    <div class="row">
+      <div class="col-md-12"><h1>Thomas Bockhorn</h1></div>
+      <div class="col-md-12">
+        <div>113 Sweet Hollow Way NW</div>
+        <div>Madison, AL 35757</div>
+        <div>Thomas.Bockhorn@tecktonet.com</div>
+      </div>
     </div>
+    <div class="row mt-5">
+      <div class="col-md-12 text-center"><h2>Full Stack Web Developer</h2></div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-md-12 text-center">
+        <div class="col-mid-12"><h3>Professional Endorsement</h3></div>
+        <p>
+          Where do I begin with Thomas!? He’s incredible! I enjoyed working
+          alongside him during my time with Whole Foods. He was an amazing
+          coworker and mentor during my time in the Meat and Seafood department
+          — he taught me so much! Aside from his role at Whole Foods, Thomas’
+          work is amazing. He’s one of the best web developers I’ve known. He
+          worked with me on a personal project and was super knowledgeable about
+          all the things I needed in terms of coding and had an idea up and
+          running within a matter of days regarding the bones of the app. He’s
+          such a pleasure to work with and he values feedback and is
+          continuously learning and growing. His continual hunger for growth is
+          one of the main reasons I recommend working with Thomas whenever you
+          get the chance! - Kristin Turner, Aerospace Welding Technician at Blue
+          Origin
+        </p>
+      </div>
+      <div class="row mt-5">
+        <div><h3>Skills</h3></div>
+        <div>
+          <p>
+            PHP, Laravel, Javascript, HTML 5, CSS 3, GitHub, Git, VueJS, JQuery,
+            WordPress, MySQL, OOP, PHPUnit, Java, Spring
+          </p>
+        </div>
+        <div><h3>Awards</h3></div>
+        <div>
+          <p>
+            Udacity Core Java Certification, Udemy web development
+            certifications
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="mt-5"><h3>Experience</h3></div>
+        <div class="mt-5"><h4>Full Stack Web Developer / Freelancer</h4></div>
+        <div><h5>1/1/2019 - Present</h5></div>
+        <div>
+          <p>
+            Full stack Web Developer providing services aimed to help my clients
+            achieve their goals. I also enjoy the challenge of building and
+            improving websites.
+          </p>
+        </div>
+        <div class="mt-5"><h4>Whole Foods Market, Huntsville Alabama</h4></div>
+        <div><h5>November 2016 - Present</h5></div>
+        <div>
+          <p>
+            I work in the meat/seafood department in Wholefoods. I provide
+            customer service and follow all the protocols that the job requires.
+          </p>
+        </div>
+        <div class="mt-5"><h4>Walmart, Huntsville Alabama</h4></div>
+        <div><h5>April 2010 - November 2016</h5></div>
+        <div>
+          <p>Provided customer service, promoted as department manager.</p>
+        </div>
+      </div>
+      <div class="col"></div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="mt-5"><h3>Education</h3></div>
+        <div class="mt-5"><h4>Udacity - Core Java</h4></div>
+        <div><p>2021</p></div>
+        <div class="mt-3"><h4>Udacity - Front-End Web Development</h4></div>
+        <div><p>2018</p></div>
+        <div class="mt-3"><h4>Udemy - Web Development Courses</h4></div>
+        <div><p>2017</p></div>
+        <div class="mt-3">
+          <h4>University of Alabama, Huntsville - Master Degree</h4>
+        </div>
+        <div><p>2010 - History Degree</p></div>
+        <div class="mt-3">
+          <h4>Montclair State University - Bachelor Degree</h4>
+        </div>
+        <div><p>2001 - Computer Science</p></div>
+      </div>
+      <div class="col"></div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="mt-5"><h3>Projects</h3></div>
+        <div class="mt-3">
+          <p>
+            See Github:
+            <a href="https://github.com/ThomasBockhorn"
+              >https://github.com/ThomasBockhorn</a
+            >
+          </p>
+        </div>
+      </div>
+      <div class="col"></div>
+    </div>
+  </div>
 </template>
 
 <script>
--- frontend/thomas_portfolio_front/src/components/components/headerFooter/Footer.vue
+++ frontend/thomas_portfolio_front/src/components/components/headerFooter/Footer.vue
@@ -3,14 +3,11 @@
     <div class="container-fluid">
       <div class="position-absolute top-50 start-50 translate-middle">
         <nav class="nav">
-          <a class="nav-link text-white" href="#">
+          <a class="nav-link text-white" href="https://www.linkedin.com/in/thomasbockhorn/">
             <i class="fab fa-linkedin fa-2x"></i>
           </a>
-          <a class="nav-link text-white" href="#">
+          <a class="nav-link text-white" href="https://github.com/ThomasBockhorn">
             <i class="fab fa-github-square fa-2x"></i>
-          </a>
-          <a class="nav-link text-white" href="#">
-            <i class="fab fa-instagram-square fa-2x"></i>
           </a>
         </nav>
       </div>
