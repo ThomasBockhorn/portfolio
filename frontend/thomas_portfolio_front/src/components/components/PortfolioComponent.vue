abdc7c6764ad575dc94f28243434aab2ce3c2ba3
Thomas <thomas.bockhorn@tecktonet.com>
2022-02-12 03:48:59 -0600

    moved project, created portfolio component

---
+++ frontend/thomas_portfolio_front/src/components/components/PortfolioComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div id="portfolio">
+      <div class="container-fluid mt-5">
+        <nav class="navbar navbar-light bg-light">
+          <div class="container-fluid">
+            <form class="d-flex mt-3">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Project"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </nav>
+        <div
+          class="d-flex flex-wrap overflow-auto justify-content-center"
+          v-if="downLoadReady"
+        >
+          <div v-for="project in projects" :key="project.id">
+            <Project :project="project"></Project>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Project from "../components/projects/Project.vue";
+
+export default {
+  data() {
+    return {
+      projects: [],
+      downLoadReady: false,
+      location: "portfolio",
+    };
+  },
+  components: {
+    Project,
+  },
+  async mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get("http://127.0.0.1:8000/api/guest/projects")
+        .then((response) => {
+          this.projects = response.data.data;
+        })
+        .catch((e) => {
+          console.log(e);
+        })
+        .finally(() => {
+          this.downLoadReady = true;
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
