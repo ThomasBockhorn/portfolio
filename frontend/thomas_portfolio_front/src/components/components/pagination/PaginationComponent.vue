771e78c683d1a35e81bbd58a0386a2f67ca9afca
Thomas <thomas.bockhorn@tecktonet.com>
2022-02-20 06:15:16 -0600

    Users can move up and down pagination

--- frontend/thomas_portfolio_front/src/components/components/pagination/PaginationComponent.vue
+++ frontend/thomas_portfolio_front/src/components/components/pagination/PaginationComponent.vue
@@ -2,7 +2,7 @@
   <nav aria-label="Page navigation example">
     <ul class="pagination">
       <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
+        <a class="page-link" @click="moveDown" aria-label="Previous">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
@@ -12,7 +12,7 @@
         </li>
       </div>
       <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
+        <a class="page-link" @click="moveUp" aria-label="Next">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
@@ -24,12 +24,28 @@
 export default {
   data() {
     return {
+      currentPage: 0
     };
   },
   props: ['totalPages'],
   methods:{
     returnPage(page){
+      this.currentPage = page;
       this.$emit('clicked', page);
+    },
+    moveUp(){
+      if(this.currentPage < this.totalPages){
+        this.returnPage(this.currentPage + 1);
+      } else{
+        this.returnPage(1);
+      }
+    },
+    moveDown(){
+      if(this.currentPage > 1){
+        this.returnPage(this.currentPage - 1);
+      }else{
+        this.returnPage(this.totalPages);
+      }
     }
   }
 };
