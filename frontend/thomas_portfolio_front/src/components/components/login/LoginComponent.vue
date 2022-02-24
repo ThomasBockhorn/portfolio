79ab85b17deae30da2350c60d754499d7d10bf8b
Thomas <thomas.bockhorn@tecktonet.com>
2022-02-24 09:57:51 -0600

    eliminated unnecessary form components

--- frontend/thomas_portfolio_front/src/components/components/login/LoginComponent.vue
+++ frontend/thomas_portfolio_front/src/components/components/login/LoginComponent.vue
@@ -2,11 +2,11 @@
   <div>
     <form>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <label for="InputEmail1" class="form-label">Email address</label>
         <input
           type="email"
           class="form-control"
-          id="exampleInputEmail1"
+          id="InputEmail1"
           aria-describedby="emailHelp"
         />
         <div id="emailHelp" class="form-text">
@@ -20,10 +20,6 @@
           class="form-control"
           id="exampleInputPassword1"
         />
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
