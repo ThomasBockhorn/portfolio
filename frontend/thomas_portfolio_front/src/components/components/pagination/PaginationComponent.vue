4ec68fdddbaa5127c0b7e3d99183b9f2ad996f5a
Thomas <thomas.bockhorn@tecktonet.com>
2022-02-27 08:39:23 -0600

    changed the background color of pagination

--- frontend/thomas_portfolio_front/src/components/components/pagination/PaginationComponent.vue
+++ frontend/thomas_portfolio_front/src/components/components/pagination/PaginationComponent.vue
@@ -1,14 +1,25 @@
 <template>
-  <nav aria-label="Page navigation example">
+  <nav aria-label="Page navigation">
     <ul class="pagination">
       <li class="page-item">
-        <a class="page-link" @click.prevent="moveDown" href="#" aria-label="Previous">
+        <a
+          class="page-link"
+          @click.prevent="moveDown"
+          href="#"
+          aria-label="Previous"
+        >
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
       <div v-for="page in totalPages" :key="page">
         <li class="page-item">
-          <a class="page-link" href="#" :class="{inActive: !currentPage}" @click.prevent="returnPage(page)">{{ page }}</a>
+          <a
+            class="page-link"
+            href="#"
+            :class="{ inActive: !currentPage }"
+            @click.prevent="returnPage(page)"
+            >{{ page }}</a
+          >
         </li>
       </div>
       <li class="page-item">
@@ -24,32 +35,41 @@
 export default {
   data() {
     return {
-      currentPage: 0
+      currentPage: 0,
     };
   },
-  props: ['totalPages'],
-  methods:{
-    returnPage(page){
+  props: ["totalPages"],
+  methods: {
+    returnPage(page) {
       this.currentPage = page;
-      this.$emit('clicked', page);
+      this.$emit("clicked", page);
     },
-    moveUp(){
-      if(this.currentPage < this.totalPages){
+    moveUp() {
+      if (this.currentPage < this.totalPages) {
         this.returnPage(this.currentPage + 1);
-      } else{
+      } else {
         this.returnPage(1);
       }
     },
-    moveDown(){
-      if(this.currentPage > 1){
+    moveDown() {
+      if (this.currentPage > 1) {
         this.returnPage(this.currentPage - 1);
-      }else{
+      } else {
         this.returnPage(this.totalPages);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
+nav {
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(40, 97, 123, 1) 51%,
+    rgba(40, 34, 100, 1) 100%
+  );
+}
 </style>
