2768664406b630cc58c440f164be46f1e3753b43
Thomas <thomas.bockhorn@tecktonet.com>
2022-02-27 06:39:14 -0600

    Added overlay to exit modal

--- frontend/thomas_portfolio_front/src/components/components/login/exit-warning/ExitWarning.vue
+++ frontend/thomas_portfolio_front/src/components/components/login/exit-warning/ExitWarning.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="modal" tabindex="-1">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Exit</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-            @click="close"
-          ></button>
-        </div>
-        <div class="modal-body">
-            Are you sure you want to Exit?
-        </div>
-        <div class="modal-footer">
+  <div class="modal-overlay">
+    <div class="modal" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Exit</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+              @click="close"
+            ></button>
+          </div>
+          <div class="modal-body">Are you sure you want to Exit?</div>
+          <div class="modal-footer">
             <button class="btn btn-warning">Yes</button>
             <button class="btn btn-primary" @click="close">No</button>
+          </div>
         </div>
       </div>
     </div>
@@ -37,5 +37,17 @@ export default {
 <style scoped>
 .modal {
   display: block;
+}
+
+.modal-overlay {
+  position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
