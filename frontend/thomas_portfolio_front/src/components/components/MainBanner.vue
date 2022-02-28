8d3904e3ca818d04af8fac026639ec32c9155533
Thomas <thomas.bockhorn@tecktonet.com>
2022-02-27 18:40:26 -0600

    changed the color of the light

--- frontend/thomas_portfolio_front/src/components/components/MainBanner.vue
+++ frontend/thomas_portfolio_front/src/components/components/MainBanner.vue
@@ -88,12 +88,12 @@ i {
 }
 @keyframes glowing {
     from {
-      box-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #f0f, 0 0 40px #0ff,
-        0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+      box-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #0ff,
+        0 0 50px #fff, 0 0 60px #fff, 0 0 70px #fff;
     }
     to {
-      box-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6,
-        0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
+      box-shadow: 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #fff,
+        0 0 50px #fff, 0 0 60px #fff, 0 0 70px #fff, 0 0 80px #fff;
     }
 
 }
