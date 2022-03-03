4721e9a2b09500af1d2ee87c8a315016b6020e4d
Thomas <thomas.bockhorn@tecktonet.com>
2022-03-03 09:09:00 -0600

    User can register into the system

--- frontend/thomas_portfolio_front/src/components/components/login/registration-form/RegistrationForm.vue
+++ frontend/thomas_portfolio_front/src/components/components/login/registration-form/RegistrationForm.vue
@@ -7,6 +7,7 @@
         class="form-control"
         id="name"
         aria-describedby="name"
+        v-model="name"
       />
       <label for="InputEmail1" class="form-label">Email:</label>
       <input
@@ -14,11 +15,17 @@
         class="form-control"
         id="InputEmail1"
         aria-describedby="email"
+        v-model="email"
       />
     </div>
     <div class="mb-3">
       <label for="InputPassword" class="form-label">Password:</label>
-      <input type="password" class="form-control" id="InputPassword" />
+      <input
+        type="password"
+        class="form-control"
+        id="InputPassword"
+        v-model="password"
+      />
     </div>
     <label for="VerifyPassword" class="form-label">Verify Password:</label>
     <input
@@ -26,18 +33,47 @@
       class="form-control"
       id="VerifyPassword"
       aria-describedby="verify passwordl"
+      v-model="verifyPassword"
     />
-    <button type="submit" class="btn btn-outline-success mt-3 mb-3">Submit</button>
+    <button
+      type="submit"
+      class="btn btn-outline-success mt-3 mb-3"
+      @click="submitRegistration"
+    >
+      Submit
+    </button>
   </form>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
       name: "",
       email: "",
+      password: "",
+      verifyPassword: "",
+      token: "",
     };
+  },
+  methods: {
+    submitRegistration(e) {
+      e.preventDefault();
+      axios
+        .post("http://127.0.0.1:8000/api/register", {
+          name: this.name,
+          email: this.email,
+          password: this.password,
+        })
+        .then((response) => {
+          this.token = response.data.data.token;
+        })
+        .catch((error) => {
+          console.log("Error: ", error.response.data.data);
+        });
+    },
   },
 };
 </script>
