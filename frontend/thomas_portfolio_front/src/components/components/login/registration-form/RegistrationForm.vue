d3ed16a2424fba384a2c65c00717707c167323bc
Thomas <thomas.bockhorn@tecktonet.com>
2022-03-03 08:04:41 -0600

    Changed form structure in registration

--- frontend/thomas_portfolio_front/src/components/components/login/registration-form/RegistrationForm.vue
+++ frontend/thomas_portfolio_front/src/components/components/login/registration-form/RegistrationForm.vue
@@ -2,32 +2,44 @@
   <form>
     <div class="mb-3">
       <label for="name">Name:</label>
-      <input type="text" class="form-control" id="name" aria-describedby="name">
-      <label for="InputEmail1" class="form-label">Email address</label>
+      <input
+        type="text"
+        class="form-control"
+        id="name"
+        aria-describedby="name"
+      />
+      <label for="InputEmail1" class="form-label">Email:</label>
       <input
         type="email"
         class="form-control"
         id="InputEmail1"
         aria-describedby="email"
       />
-      <label for="VerifyEmail1" class="form-label">Verify Email address</label>
-      <input
-        type="email"
-        class="form-control"
-        id="VerifyEmail1"
-        aria-describedby="verify-email"
-      />
     </div>
     <div class="mb-3">
-      <label for="InputPassword" class="form-label">Password</label>
+      <label for="InputPassword" class="form-label">Password:</label>
       <input type="password" class="form-control" id="InputPassword" />
     </div>
-    <button type="submit" class="btn btn-outline-success">Submit</button>
+    <label for="VerifyPassword" class="form-label">Verify Password:</label>
+    <input
+      type="password"
+      class="form-control"
+      id="VerifyPassword"
+      aria-describedby="verify passwordl"
+    />
+    <button type="submit" class="btn btn-outline-success mt-3 mb-3">Submit</button>
   </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+    };
+  },
+};
 </script>
 
 <style>
