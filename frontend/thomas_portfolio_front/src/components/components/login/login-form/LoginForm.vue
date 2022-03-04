4f56333aa55d8d4af7f9a46b60d04d94fbdfc465
Thomas <thomas.bockhorn@tecktonet.com>
2022-03-04 11:33:43 -0600

    Users can login

--- frontend/thomas_portfolio_front/src/components/components/login/login-form/LoginForm.vue
+++ frontend/thomas_portfolio_front/src/components/components/login/login-form/LoginForm.vue
@@ -7,20 +7,56 @@
         class="form-control"
         id="InputEmail1"
         aria-describedby="emailHelp"
+        v-model="email"
       />
     </div>
     <div class="mb-3">
       <label for="exampleInputPassword1" class="form-label">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" />
+      <input
+        type="password"
+        class="form-control"
+        id="exampleInputPassword1"
+        v-model="password"
+      />
     </div>
-    <button type="submit" class="btn btn-outline-success">Submit</button>
+    <button
+      type="submit"
+      class="btn btn-outline-success"
+      @click="loginSubmition"
+    >
+      Submit
+    </button>
   </form>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    loginSubmition(e) {
+      e.preventDefault();
+      axios
+        .post("http://127.0.0.1:8000/api/login", {
+          email: this.email,
+          password: this.password,
+        })
+        .then((response) => {
+          console.log(response.data.data.token);
+        })
+        .catch((error) => {
+          console.log("Error: ", error.response.data.data);
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
