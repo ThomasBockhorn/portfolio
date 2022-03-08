e4148b4a1b40424802b7fd78efbda90c1455da92
Thomas <thomas.bockhorn@tecktonet.com>
2022-03-08 12:22:07 -0600

    Added comment to portfolio page

--- frontend/thomas_portfolio_front/src/components/pages/Portfolio.vue
+++ frontend/thomas_portfolio_front/src/components/pages/Portfolio.vue
@@ -1,29 +1,29 @@
 <template>
-    <div id="mainPortfolio">
-        <MainBanner v-bind:mainComponentName="this.location"></MainBanner>
-        <PortfolioComponent></PortfolioComponent>
-    </div>
+  <div id="mainPortfolio">
+    <MainBanner v-bind:mainComponentName="this.location"></MainBanner>
+    <PortfolioComponent></PortfolioComponent>
+  </div>
 </template>
 
 <script>
 import PortfolioComponent from "../components/portfolio/PortfolioComponent.vue";
 import MainBanner from "../components/mainBanner/MainBanner.vue";
 
+/**
+ * Portfolio page
+ */
 export default {
-    components: {
-        MainBanner,
-        PortfolioComponent
-    },
-     data(){
-        return{
-            location: "portfolio"
-        }
-    }
-    
-    
+  components: {
+    MainBanner,
+    PortfolioComponent,
+  },
+  data() {
+    return {
+      location: "portfolio",
+    };
+  },
 };
 </script>
 
 <style scoped>
-
 </style>
