c990db637e0656fcc0e97cc0ed8bb4701a31a86e
Thomas <thomas.bockhorn@tecktonet.com>
2022-03-08 12:16:28 -0600

    Added comment and refactored comment page

--- frontend/thomas_portfolio_front/src/components/pages/Contact.vue
+++ frontend/thomas_portfolio_front/src/components/pages/Contact.vue
@@ -1,25 +1,26 @@
 <template>
-    <div>
-        <MainBanner v-bind:mainComponentName="this.location"></MainBanner>
-        <ContactComponent></ContactComponent>
-    </div>
+  <div>
+    <MainBanner v-bind:mainComponentName="this.location"></MainBanner>
+    <ContactComponent></ContactComponent>
+  </div>
 </template>
 
 <script>
 import MainBanner from "../components/mainBanner/MainBanner.vue";
 import ContactComponent from "../components/contact/ContactComponent.vue";
 
+/**
+ * Contact page
+ */
 export default {
-    components:{
-        MainBanner,
-        ContactComponent
-    },
-    data(){
-        return{
-            location: "contact"
-        }
-    }
+  components: {
+    MainBanner,
+    ContactComponent,
+  },
+  data() {
+    return {
+      location: "contact",
+    };
+  },
 };
 </script>
-
-<style></style>
