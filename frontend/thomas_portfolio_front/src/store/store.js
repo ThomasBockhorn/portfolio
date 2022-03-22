a9b9fa809c3c0ff8f3490b518907df3fbecf2f85
Thomas <thomas.bockhorn@tecktonet.com>
2022-03-21 22:04:18 -0500

    Added persisted state plugin to vuex

--- frontend/thomas_portfolio_front/src/store/store.js
+++ frontend/thomas_portfolio_front/src/store/store.js
@@ -3,12 +3,16 @@ import portfolio from './module/portfolio/index';
 import usersRegistration from './module/usersRegistration/index';
 import userLogin from './module/userLogin/index';
 import crudOperations from './module/crudOperations/index';
+import createPersistedState from 'vuex-persistedstate'
 
 /**
  * Vuex Store
  */
 
 export const store = createStore({
+    plugins: [
+        createPersistedState()
+    ],
     modules: {
         portfolio,
         usersRegistration,
