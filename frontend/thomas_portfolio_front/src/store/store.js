c11a8a807ebeab1a8664474ebad76f23c3d469ab
Thomas <thomas.bockhorn@tecktonet.com>
2022-03-28 12:26:47 -0500

    Renamed AuthOperations in store

--- frontend/thomas_portfolio_front/src/store/store.js
+++ frontend/thomas_portfolio_front/src/store/store.js
@@ -2,7 +2,7 @@ import { createStore } from 'vuex';
 import portfolio from './module/portfolio/index';
 import usersRegistration from './module/usersRegistration/index';
 import userLogin from './module/userLogin/index';
-import crudOperations from './module/crudOperations/index';
+import crudOperations from './module/authOperations/index';
 import createPersistedState from 'vuex-persistedstate'
 
 /**
